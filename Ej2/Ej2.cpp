5fba878358f5ab72be2c836631ae357a66b92591
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-10-08 14:25:30 -0300

    Update Ej2.cpp

--- Ej2/Ej2.cpp
+++ Ej2/Ej2.cpp
@@ -1,3 +1,7 @@
+/*Dado el archivo VENTAS.DAT, ordenado por número de factura,  con la siguiente estructura de registro:  Número de factura (int) , Código de vendedor (int) , Importe (float) 
+a) Imprima un listado ordenado por código de vendedor, con código de vendedor y el detalle de cada venta, ordenadas de mayor a menor por importe. 
+b) Genere el archivo “VentasVendedor.dat” ordenado por código de vendedor, con código de vendedor y el importe total de ventas*/
+
 #include <iostream>
 #include <stdio.h>
 
@@ -112,7 +116,6 @@ void InsertaSubLista(NodoSub*&SubLista,Importes Dato)
         SubLista = Nuevo;
     }
 }
-
 
 void MostrarListado(FILE *ArchivoB,Nodo *Lista)
 {

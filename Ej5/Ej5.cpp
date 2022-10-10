a7002f903707f14f8bb594d610b9d87faaa9c133
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-10-10 18:12:43 -0300

    Add files via upload

---
+++ Ej5/Ej5.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+struct CobranzasDelArchivo
+{
+    int CodDeCliente,NroDeCuota;
+};
+
+struct Cuotas
+{
+    int NroDeCuota;
+};
+
+struct NodoSub
+{
+    Cuotas Info;
+    NodoSub *Sgte;
+};
+
+struct Cliente
+{
+    int CodDeCliente;
+    NodoSub *ListaDeCuotas;
+};
+
+struct Nodo
+{
+    Cliente Info;
+    Nodo *Sgte;
+};
+
+void CargaDeDatos(FILE*ArchivoA,Nodo*&Lista);
+void InsertaSubLista(NodoSub*&ListaDeCuotas,Cliente Dato);
+Nodo *BuscarInsertar(Nodo*&Lista,Cliente Dato);
+
+int main()
+{
+    Nodo *Lista = NULL;
+    FILE *ArchivoA = fopen("Cobranzas.dat","rb");
+
+    if(ArchivoA == NULL)
+    {
+        cout << "ERROR" << endl;
+    }
+    else
+    {
+        CargaDeDatos(ArchivoA,Lista);
+
+        cout << "Listado de aquellos que adeudan cuotas: " << endl;
+
+        MostrarListado(Lista);
+
+        cout << "---------------------------------" << endl;
+    }
+
+    return 0;
+}
+
+void CargaDeDatos(FILE*ArchivoA,Nodo*&Lista)
+{
+    CobranzasDelArchivo C;
+    Cliente AuxCliente;
+    Cuotas AuxCoutas;
+    Nodo *Aux;
+
+    fread(&C,sizeof(CobranzasDelArchivo),1,ArchivoA);
+
+    while(!feof(ArchivoA))
+    {
+        AuxCliente.CodDeCliente = C.CodDeCliente;
+        AuxCoutas.NroDeCuota = C.NroDeCuota;
+        AuxCliente.ListaDeCuotas = NULL;
+        Aux = BuscarInsertar(Lista,AuxCliente);
+
+        InsertarCuota(Aux->Info.ListaDeCuotas,AuxCoutas);
+
+        fread(&C,sizeof(CobranzasDelArchivo),1,ArchivoA);
+    }
+}
+
+void InsertaSubLista(NodoSub*&ListaDeCuotas,Cliente Dato)
+{
+    NodoSub *Nuevo,*Antecesor,*Aux;
+    Nuevo = new NodoSub;
+    Nuevo->Info = Dato;
+    Aux = ListaDeCuotas;
+
+    while(Aux != NULL && Aux->Info.NroDeCuota < Dato.NroDeCuota)
+    {
+        Antecesor = Aux;
+        Aux = Aux->Sgte;
+    }
+
+    Nuevo->Sgte = Aux;
+
+    if(Aux != ListaDeCuotas)
+    {
+        Antecesor->Sgte = Nuevo;
+    }
+    else
+    {
+        ListaDeCuotas = Nuevo;
+    }
+}
+
+Nodo *BuscarInsertar(Nodo*&Lista,Cliente Dato)
+{
+    Nodo *Aux,*Antecesor;
+    Aux = Lista;
+
+    while(Aux != NULL && Aux->Info.CodDeCliente < Dato.CodDeCliente)
+    {
+        Antecesor = Aux;
+        Aux = Aux->Sgte;
+    }
+
+    if(Aux != NULL && Dato.CodDeCliente == Aux->Info.CodDeCliente)
+    {
+        return Aux;
+    }
+    else
+    {
+        Nodo *Nuevo = new Nodo;
+        Nuevo->Info = Dato;
+        Nuevo->Sgte = Aux;
+
+        if(Aux != Lista)
+        {
+            Antecesor->Sgte = Nuevo;
+        }
+        else
+        {
+            Lista = Nuevo;
+        }
+
+        return Nuevo;
+    }
+}

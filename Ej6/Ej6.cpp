8590c402ebb80dbcb74f4c1d19b769d59d0add20
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-10-10 18:15:43 -0300

    Update Ej6.cpp

--- Ej6/Ej6.cpp
+++ Ej6/Ej6.cpp
@@ -1,3 +1,10 @@
+/*
+La ciudad de Buenos Aires requiere un estudio estadístico de infracciones realizadas por los vehículos en sus calles. 
+Para ello cuenta con un archivo de infracciones “Infracciones.dat”, ordenado por fecha de la infracción creciente, con el siguiente diseño:  
+    Patente (7 caracteres) , Fecha de Infracción (int, aammdd) , Código de infracción (int, 1 a 100)   
+Se desea obtener un listado ordenado por código de infracción decreciente con las  patentes que cometieron más de una vez la misma infracción.
+*/
+
 #include <iostream>
 #include <stdio.h>
 
